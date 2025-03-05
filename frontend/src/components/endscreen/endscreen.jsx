80ae6e35007eca24da34e683d117eeaa6c1cb79f
jonathandahl <jonathan@DESKTOP-I8AR90E.localdomain>
2025-03-05 12:51:37 +0200

    endscreen

--- frontend/src/components/endscreen/endscreen.jsx
+++ frontend/src/components/endscreen/endscreen.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const EndScreen = ({ onQuit }) => {
     return (
-        <div>
-            <div className="flex flex-col border-2 border-black text-white bg-gray-800 p-10 rounded-lg">
-            <button
-                onClick={onQuit}
-                className="px-6 py-3 bg-red-500 hover:bg-red-700 text-white font-bold rounded-lg transition m-2"
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex flex-col justify-center items-center">
+            <div className="bg-gray-800 text-white p-10 rounded-lg">
+                <h1 className="text-3xl mb-4">Game Over!</h1>
+                <button
+                    onClick={onQuit}
+                    className="px-6 py-3 bg-red-500 hover:bg-red-700 text-white font-bold rounded-lg transition m-2"
                 >
-                Quit
-             </button>
+                    Quit
+                </button>
             </div>
         </div>
     )
